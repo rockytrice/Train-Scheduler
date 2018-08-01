f98ffd8bfce4464458896c47d06813c6a13deead
Roderick Trice <Rodericktrice@Rodericks-MacBook-Pro.local>
2018-08-01 19:09:48 -0400

    data is logging to the console and to the database

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -2,15 +2,39 @@ console.log("connected");
 
 
 
- // Initialize Firebase
- var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyBL01dBTjppMuY0Rmb5XFrQ901hZL59dPk",
     authDomain: "train-schedule-97e18.firebaseapp.com",
     databaseURL: "https://train-schedule-97e18.firebaseio.com",
     projectId: "train-schedule-97e18",
     storageBucket: "train-schedule-97e18.appspot.com",
     messagingSenderId: "804451989933"
-  };
-  firebase.initializeApp(config);
-// creating a variable to refence the datadase.
-  var database = firebase.database();
+};
+firebase.initializeApp(config);
+// creating a variable to reference the database.
+var database = firebase.database();
+
+// this is the button for adding new train 
+$("#add-train-btn").on("click", function (event) {
+    event.preventDefault();
+
+    var trainName = $("#name-input").val().trim();
+    var trainDestination = $("#Destination-input").val().trim();
+    var trainTime = $("#train-time").val().trim();
+    var trainFrequency = $("#Frequency-input").val().trim();
+
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        time: trainTime,
+        frequency: trainFrequency
+
+    }
+
+    database.ref().push(newTrain);
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(trainTime);
+    console.log(trainFrequency);
+})
