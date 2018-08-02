fc83c88fc4186b746fa7d568b8fbdbbbacc508e5
Roderick Trice <Rodericktrice@Rodericks-MacBook-Pro.local>
2018-08-01 21:31:32 -0400

    testing off frequency and arrival time seems to be good

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,7 +1,3 @@
-console.log("connected");
-
-
-
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyBL01dBTjppMuY0Rmb5XFrQ901hZL59dPk",
@@ -21,9 +17,10 @@ $("#add-train-btn").on("click", function (event) {
 
     var trainName = $("#name-input").val().trim();
     var trainDestination = $("#Destination-input").val().trim();
-    var trainTime = moment($("#train-time").val().trim(), "HH:mm" ).format("hh:mm:ss");
+    var trainTime = moment($("#train-time").val().trim(), "HH:mm").format("hh:mm:ss");
     var trainFrequency = $("#Frequency-input").val().trim();
 
+    // storing into variables
     var newTrain = {
         name: trainName,
         destination: trainDestination,
@@ -31,7 +28,7 @@ $("#add-train-btn").on("click", function (event) {
         frequency: trainFrequency
 
     }
-// logs to the console.. working properly
+    // logs to the console.. working properly
     database.ref().push(newTrain);
     console.log(trainName);
     console.log(trainDestination);
@@ -39,14 +36,14 @@ $("#add-train-btn").on("click", function (event) {
     console.log(trainFrequency);
 
 
-// clears input field when user submits data
+    // clears input field when user submits data
     $("#name-input").val("");
     $("#Destination-input").val("");
     $("#train-time").val("");
     $("#Frequency-input").val("");
 })
-
-database.ref().on("child_added", function(childSnapShot){
+// firebase event for adding the new train to the database
+database.ref().on("child_added", function (childSnapShot) {
     console.log(childSnapShot.val());
 
     var trainName = childSnapShot.val().name;
@@ -60,4 +57,32 @@ database.ref().on("child_added", function(childSnapShot){
     console.log(trainTime);
     console.log(trainFrequency);
 
-})
+
+    $(".name-data").append(trainName);
+
+
+
+    var firstTimeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm:ss"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % trainFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = trainFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+});
